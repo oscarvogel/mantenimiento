04fe293c2c6c24852b1daea9afa2a78c1dd83e61
Oscar Vogel <oscarvogel@gmail.com>
2026-08-20 16:03:07 -0300

    fix: montar generador demo en superadmin activo (#98)

---
+++ frontend/src/pages/admin/SuperAdminDemoPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SuperAdminPage from './SuperAdminPage.vue'
+import DemoCompanyPanel from './components/DemoCompanyPanel.vue'
+
+defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div>
+    <SuperAdminPage :data="data" />
+    <DemoCompanyPanel :data="data" />
+  </div>
+</template>
