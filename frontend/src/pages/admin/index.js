98f8fdd04f284d5b4eec71a20498ae6ceee7bbfc
Oscar Vogel <oscarvogel@gmail.com>
2026-08-23 20:08:48 -0300

    feat(chatbot): registrar pagina de auditoria

--- frontend/src/pages/admin/index.js
+++ frontend/src/pages/admin/index.js
@@ -1,12 +1,14 @@
 import BranchesAdminPage from './BranchesAdminPage.vue'
+import ChatAuditPage from './ChatAuditPage.vue'
 import SuperAdminPage from './SuperAdminPage.vue'
 import SuperAdminDemoPage from './SuperAdminDemoPage.vue'
 import UsersAdminPage from './UsersAdminPage.vue'
 
-export { BranchesAdminPage, SuperAdminPage, SuperAdminDemoPage, UsersAdminPage }
+export { BranchesAdminPage, ChatAuditPage, SuperAdminPage, SuperAdminDemoPage, UsersAdminPage }
 
 export const adminPagesByType = Object.freeze({
   superadmin: SuperAdminDemoPage,
   'branches-admin': BranchesAdminPage,
   'users-admin': UsersAdminPage,
+  'chatbot-audit': ChatAuditPage,
 })
