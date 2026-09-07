f2c06549917e4a1bbb2e5a9742bd07dfcc5a487d
Oscar Vogel <oscarvogel@gmail.com>
2026-09-07 17:23:54 -0300

    feat(importacion): agregar ruta de preview de choferes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -130,6 +130,7 @@ $routes->group('mantenimiento', ['filter' => ['auth']], static function ($routes
     $routes->post('importaciones/biblioteca/servicios/(:num)/tareas/nueva', 'LibraryTaskCatalog::createAndLink/$1', ['filter' => 'permission:importaciones.cargar']);
     $routes->get('importaciones/plantilla/(:segment)', 'ImportManagement::template/$1', ['filter' => 'permission:importaciones.cargar']);
     $routes->post('importaciones', 'ImportManagement::upload', ['filter' => 'permission:importaciones.cargar']);
+    $routes->post('importaciones/choferes/preview', 'ImportManagement::driverAssignmentsPreview', ['filter' => ['permission:importaciones.cargar', 'permission:empleados.editar']]);
     $routes->get('importaciones/(:num)', 'ImportManagement::show/$1', ['filter' => 'permission:importaciones.ver']);
     $routes->post('importaciones/(:num)/confirmar', 'ImportManagement::confirm/$1', ['filter' => 'permission:importaciones.cargar']);
     $routes->post('importaciones/(:num)/cancelar', 'ImportManagement::cancel/$1', ['filter' => 'permission:importaciones.cargar']);
