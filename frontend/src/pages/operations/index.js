91503955fe21bbb4da79d33fb61a665043bf1c29
Oscar Vogel <oscarvogel@gmail.com>
2026-09-11 17:30:34 -0300

    feat: registra pantalla de vencimientos

--- frontend/src/pages/operations/index.js
+++ frontend/src/pages/operations/index.js
@@ -5,6 +5,7 @@ import EquipmentOperatePage from './EquipmentOperatePage.vue'
 import DriverAssignmentsPreviewPage from './DriverAssignmentsPreviewPage.vue'
 import EmployeesIndexPage from './EmployeesIndexPage.vue'
 import ExpirationTypesMasterPage from './ExpirationTypesMasterPage.vue'
+import ExpirationsIndexPage from './ExpirationsIndexPage.vue'
 import ImportsIndexPage from './ImportsIndexPage.vue'
 import ImportsShowPage from './ImportsShowPage.vue'
 import MaintenanceOverviewPage from './MaintenanceOverviewPage.vue'
@@ -24,6 +25,7 @@ export {
   DriverAssignmentsPreviewPage,
   EmployeesIndexPage,
   ExpirationTypesMasterPage,
+  ExpirationsIndexPage,
   ImportsIndexPage,
   ImportsShowPage,
   MaintenanceOverviewPage,
@@ -45,6 +47,7 @@ export const operationPageComponents = Object.freeze({
   'driver-assignments-preview': DriverAssignmentsPreviewPage,
   'employees-index': EmployeesIndexPage,
   'expiration-types-master': ExpirationTypesMasterPage,
+  'expirations-index': ExpirationsIndexPage,
   'assets-index': AssetsIndexPage,
   'imports-index': ImportsIndexPage,
   'imports-show': ImportsShowPage,
