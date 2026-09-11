854de8c48e3357cf5c8e1f717942abc886b2147b
Oscar Vogel <oscarvogel@gmail.com>
2026-09-11 18:43:16 -0300

    feat(notificaciones): registra pantalla de configuración

--- frontend/src/pages/admin/index.js
+++ frontend/src/pages/admin/index.js
@@ -1,15 +1,17 @@
 import BranchesAdminPage from './BranchesAdminPage.vue'
 import ChatAuditPage from './ChatAuditPage.vue'
+import NotificationSettingsPage from './NotificationSettingsPage.vue'
 import SuperAdminPage from './SuperAdminPage.vue'
 import SuperAdminDemoPage from './SuperAdminDemoPage.vue'
 import UsersAdminPage from './UsersAdminPage.vue'
 import UsersAdminWithAuditPage from './UsersAdminWithAuditPage.vue'
 
-export { BranchesAdminPage, ChatAuditPage, SuperAdminPage, SuperAdminDemoPage, UsersAdminPage, UsersAdminWithAuditPage }
+export { BranchesAdminPage, ChatAuditPage, NotificationSettingsPage, SuperAdminPage, SuperAdminDemoPage, UsersAdminPage, UsersAdminWithAuditPage }
 
 export const adminPagesByType = Object.freeze({
   superadmin: SuperAdminDemoPage,
   'branches-admin': BranchesAdminPage,
   'users-admin': UsersAdminWithAuditPage,
   'chatbot-audit': ChatAuditPage,
+  'superadmin-notification-settings': NotificationSettingsPage,
 })
