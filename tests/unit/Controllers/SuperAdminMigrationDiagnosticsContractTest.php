08cbcf69dc53ac0f8457fcae7475bd7d8ed2f268
Oscar Vogel <oscarvogel@gmail.com>
2026-09-18 11:19:19 -0300

    Fix #323: reparación V2 de vencimientos duplicados en Ferozo
    * fix: detectar migraciones en Ferozo con scandir
    
    * fix: agregar reparación V2 de vencimientos activos duplicados
    
    * test: cubrir descubrimiento Ferozo y migración V2

--- tests/unit/Controllers/SuperAdminMigrationDiagnosticsContractTest.php
+++ tests/unit/Controllers/SuperAdminMigrationDiagnosticsContractTest.php
@@ -16,5 +16,7 @@ final class SuperAdminMigrationDiagnosticsContractTest extends TestCase
         self::assertStringContainsString('private function migrationDiagnostics(): array', $controller);
         self::assertStringContainsString("'target319Registered'", $controller);
         self::assertStringContainsString("'duplicateActiveGroups'", $controller);
+        self::assertStringContainsString('scandir($migrationDir)', $controller);
+        self::assertStringContainsString("2026-09-18-140500", $controller);
     }
 }
