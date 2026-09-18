4f7f0f8caf1cab25d4c60dd72ea78f694fee8eb1
Oscar Vogel <oscarvogel@gmail.com>
2026-09-18 18:13:55 -0300

    test: cubrir fallback de instancia global WhatsApp

---
+++ tests/unit/Infrastructure/Notifications/WhatsAppCompanyInstanceFallbackContractTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class WhatsAppCompanyInstanceFallbackContractTest extends TestCase
+{
+    public function testAutomaticQueueFallsBackToGlobalSettingsInstance(): void
+    {
+        $queue = file_get_contents(APPPATH . 'Infrastructure/Notifications/CodeIgniterWhatsAppNotificationDeliveryQueue.php');
+
+        self::assertIsString($queue);
+        self::assertStringContainsString("$globalSettings = \$this->settings->get();", $queue);
+        self::assertStringContainsString("$globalSettings['whatsapp_instance_id']", $queue);
+        self::assertStringNotContainsString("env('whatsapp.instanceId'", $queue);
+    }
+}
