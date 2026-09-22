13d92a00e9fd50eea21f5dbcaac39632eef12bd0
Oscar Vogel <oscarvogel@gmail.com>
2026-09-22 17:12:04 -0300

    test: cubrir deduplicación de pruebas históricas

--- tests/unit/Infrastructure/Notifications/WhatsAppWeeklyReadingReminderContractTest.php
+++ tests/unit/Infrastructure/Notifications/WhatsAppWeeklyReadingReminderContractTest.php
@@ -56,7 +56,9 @@ final class WhatsAppWeeklyReadingReminderContractTest extends TestCase
         self::assertStringContainsString("env('alerts.whatsappBatchLimit', 5)", $controller);
         self::assertStringContainsString("env('alerts.whatsappSendIntervalMs', 2000)", $controller);
         self::assertStringContainsString('usleep($intervalMs * 1000)', $controller);
-        self::assertStringContainsString("->where('clave_entrega', $deliveryKey)", $queue);
+        self::assertStringContainsString('$baseDeliveryKey', $queue);
+        self::assertStringContainsString("->where('clave_entrega', $baseDeliveryKey)", $queue);
+        self::assertStringContainsString("->like('clave_entrega', $baseDeliveryKey . ':prueba:', 'after')", $queue);
         self::assertStringContainsString('$alreadyExists', $queue);
         self::assertStringContainsString("date('o-\\\\WW')", $controller);
         self::assertStringNotContainsString("date('YmdHis') . '-actor-'", $controller);
